b76dc89b15e5b8384778fecd5c9a7ad1c955aff2
Thibaut <tgudin@besport.com>
2021-08-20 18:00:42 +0200

    adding 'configure' and 'fetch_update' functions into the chcp library

--- src/chcp.mli
+++ src/chcp.mli
@@ -1,1 +1,29 @@
-(*TODO*)
+type configure_options
+
+val configure_options :
+  ?config_file:string ->
+  ?auto_download:bool ->
+  ?auto_install:bool ->
+  unit ->
+  configure_options
+  [@@js.builder] [@@js.verbatim_names]
+
+val configure : ?options:configure_options -> (unit -> unit) -> unit
+  [@@js.global "chcp.configure"]
+
+type update_options
+
+val update_options :
+  ?config_file:string -> ?request_headers:string list -> unit -> update_options
+  [@@js.builder] [@@js.verbatim_names]
+
+type error
+
+val code : error -> int [@@js.get]
+
+val description : error -> string [@@js.get]
+
+type data
+
+val fetch_update : ?options:update_options -> (error -> data -> unit) -> unit
+  [@@js.global "chcp.fetchUpdate"]
